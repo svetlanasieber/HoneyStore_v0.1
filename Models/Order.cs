24215f05dccae42808c0ec6c21989789d16cee31
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-01-04 18:35:21 +0100

    Create Order.cs

---
+++ Models/Order.cs
@@ -0,0 +1,10 @@
+namespace HoneyStore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
